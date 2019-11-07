0152e5c932eafbe026c08edb262c685ed5eecafb
Павел <shmachilin@yandex.ru>
2019-11-07 21:04:21 +0300

    Тестовый клиент WebAPI

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected readonly HttpClient _Client;
+
+        protected readonly string _ServiceAddress;
+
+        protected BaseClient(IConfiguration config, string ServiceAddress)
+        {
+            _ServiceAddress = ServiceAddress;
+
+            _Client = new HttpClient
+            {
+                BaseAddress = new Uri(config["ClientAddress"])
+            };
+
+            var headers = _Client.DefaultRequestHeaders.Accept;
+
+            headers.Clear();
+            headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
--- Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -6,7 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.ServiceHosting.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/values")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
