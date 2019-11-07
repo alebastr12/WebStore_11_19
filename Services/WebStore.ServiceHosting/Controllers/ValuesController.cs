aabba6acab2a58c36e43065d74bc0994a7f80ba2
Павел <shmachilin@yandex.ru>
2019-11-07 20:39:40 +0300

    Рефакторинг

--- Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -14,14 +14,14 @@ namespace WebStore.ServiceHosting.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Enumerable.Range(1, 50).Select(i => $"Value {i}").ToArray() ;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return $"value {id}";
         }
 
         // POST api/values
--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -14,20 +14,15 @@ namespace WebStore.ServiceHosting
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
