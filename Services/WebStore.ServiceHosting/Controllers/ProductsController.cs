817b2c1dd580d09df6aa2168505a09c2bc974c76
Павел <shmachilin@yandex.ru>
2019-11-11 20:43:57 +0300

    DTO-модели + изменение IProductData

---
+++ Services/WebStore.ServiceHosting/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase, IProductData
+    {
+        private readonly IProductData _ProductData;
+
+        public ProductsController(IProductData ProductData) => _ProductData = ProductData;
+
+        [HttpGet("sections")]
+        public IEnumerable<Section> GetSections() => _ProductData.GetSections();
+
+        [HttpGet("brands")]
+        public IEnumerable<Brand> GetBrands() => _ProductData.GetBrands();
+
+        [HttpPost, ActionName("Post")]
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter) => _ProductData.GetProducts(Filter);
+
+        [HttpGet("{id}"), ActionName("Get")]
+        public ProductDTO GetProductById(int id) => _ProductData.GetProductById(id);
+    }
+}
--- Services/WebStore.Services/Product/InMemoryProductData.cs
+++ Services/WebStore.Services/Product/InMemoryProductData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using WebStore.DAL.Data;
+using WebStore.Domain.DTO.Products;
 using WebStore.Domain.Entities;
 using WebStore.Interfaces.Services;
 
@@ -12,17 +13,46 @@ namespace WebStore.Services.Product
 
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
 
-        public IEnumerable<Domain.Entities.Product> GetProducts(ProductFilter Filter)
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter)
         {
             var products = TestData.Products;
-            if (Filter is null) return products;
-            if (Filter.BrandId != null)
+
+            if (Filter?.BrandId != null)
                 products = products.Where(product => product.BrandId == Filter.BrandId);
-            if (Filter.SectionId != null)
+            if (Filter?.SectionId != null)
                 products = products.Where(product => product.SectionId == Filter.SectionId);
-            return products;
+
+            return products.Select(p => new ProductDTO
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Order = p.Order,
+                Price = p.Price,
+                ImageUrl = p.ImageUrl,
+                Brand = p.Brand is null ? null : new BrandDTO
+                {
+                    Id = p.Brand.Id,
+                    Name = p.Brand.Name
+                }
+            });
         }
 
-        public Domain.Entities.Product GetProductById(int id) => TestData.Products.FirstOrDefault(product => product.Id == id);
+        public ProductDTO GetProductById(int id)
+        {
+            var p = TestData.Products.FirstOrDefault(product => product.Id == id);
+            return new ProductDTO
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Order = p.Order,
+                Price = p.Price,
+                ImageUrl = p.ImageUrl,
+                Brand = p.Brand is null ? null : new BrandDTO
+                {
+                    Id = p.Brand.Id,
+                    Name = p.Brand.Name
+                }
+            };
+        }
     }
 }
