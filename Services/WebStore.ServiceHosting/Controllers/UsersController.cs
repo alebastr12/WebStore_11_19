a846d875cb6ab6cf36347810938e1757a4384865
Павел <shmachilin@yandex.ru>
2019-11-14 19:50:00 +0300

    Подготовка контроллеров WebAPI для пользвоателей и ролей

---
+++ Services/WebStore.ServiceHosting/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserStore<User> _UserStore;
+
+        public UsersController(WebStoreContext db)
+        {
+            _UserStore = new UserStore<User>(db) { AutoSaveChanges = true };
+        }
+
+    }
+}
