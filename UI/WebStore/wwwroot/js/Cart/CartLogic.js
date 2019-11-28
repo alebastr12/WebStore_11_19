b8d0db15c8b3fdc6a1fe42a5052d2830e07b3801
Pavel <shmachilin@gmail.com>
2019-11-28 20:31:27 +0300

    Инкремент/декремент количества товаров в корзине средствами ajax

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -2,45 +2,99 @@
     _properties: {
         getCartViewLink: "",
         addToCartLink: "",
-        removeFromCartLink: ""
+        decrementLink: "",
+        removeFromCartLink: "",
+        removeAllLink: ""
     },
 
-    init: function(properties) {
+    init: function (properties) {
         $.extend(Cart._properties, properties);
 
         Cart.initEvents();
     },
 
-    initEvents: function() {
+    initEvents: function () {
         $(".add-to-cart").click(Cart.addToCart);
+        $(".cart_quantity_up").click(Cart.incrementItem);
+        $(".cart_quantity_down").click(Cart.decrementItem);
         $(".cart_quantity_delete").click(Cart.removeFromCart);
     },
 
-    addToCart: function(event) {
+    addToCart: function (event) {
         event.preventDefault();
 
         var button = $(this);
         var id = button.data("id");
 
         $.get(Cart._properties.addToCartLink + "/" + id)
-            .done(function() {
+            .done(function () {
                 Cart.showToolTip(button);
                 Cart.refreshCartView();
             })
             .fail(function () { console.log("addToCart fail"); });
     },
 
-    showToolTip: function(button) {
+    incrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        var id = button.data("id");
+        var container = button.closest("tr");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function () {
+                var count = parseInt($(".cart_quantity_input", container).val());
+                $(".cart_quantity_input", container).val(count + 1);
+                Cart.refreshPrice(container);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("incrementItem fail"); });
+    },
+
+    decrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        var id = button.data("id");
+        var container = button.closest("tr");
+
+        $.get(Cart._properties.decrementLink + "/" + id)
+            .done(function () {
+                var count = parseInt($(".cart_quantity_input", container).val());
+                if (count > 1) {
+                    $(".cart_quantity_input", container).val(count - 1);
+                    Cart.refreshPrice(container);
+                } else {
+                    container.remove();
+                    Cart.refreshTotalPrice();
+                }
+            })
+            .fail(function () { console.log("decrementItem fail"); });
+    },
+
+    refreshPrice: function (container) {
+        var quantity = parseInt($(".cart_quantity_input", container).val());
+        var price = parseFloat($(".cart_price", container).data("price"));
+        var totalPrice = quantity * price;
+
+        var value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+        $(".cart_total_price", container).data("price", totalPrice);
+        $(".cart_total_price", container).html(value);
+
+        Cart.refreshTotalPrice();
+    },
+
+    showToolTip: function (button) {
         button.tooltip({ title: "Добавлено в корзину" }).tooltip("show");
-        setTimeout(function() {
+        setTimeout(function () {
             button.tooltip("destroy");
         }, 500);
     },
 
-    refreshCartView: function() {
+    refreshCartView: function () {
         var container = $("#cart-container");
         $.get(Cart._properties.getCartViewLink)
-            .done(function(result) {
+            .done(function (result) {
                 container.html(result);
             })
             .fail(function () { console.log("refreshCartView fail"); });
@@ -52,17 +106,17 @@
         var button = $(this);
         var id = button.data("id");
         $.get(Cart._properties.removeFromCartLink + "/" + id)
-            .done(function() {
+            .done(function () {
                 button.closest("tr").remove();
                 Cart.refreshTotalPrice();
             })
             .fail(function () { console.log("removeFromCart fail"); });
     },
 
-    refreshTotalPrice: function() {
+    refreshTotalPrice: function () {
         var total = 0;
 
-        $(".cart_total_price").each(function() {
+        $(".cart_total_price").each(function () {
             var price = parseFloat($(this).data("price"));
             total += price;
         });
